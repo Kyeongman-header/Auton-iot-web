4465c3b5b37b504e5ae9c8af9c06b83feb9275ed
Kyeongman-header <zzangman2@gmail.com>
2022-02-01 21:36:27 +0900

    alpha 5

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,7 +8,7 @@ export default function Footer()
                     <h4>자동차 실내 내장형 스마트 공기 청정기 시스템의 기술 개발</h4>
                     <h5>오토앤, 고등기술연구원, 서울대학교 산학협력단 AI 연구소</h5>
                 </section>
-                <section className="pr-5">
+                <section className="pr-5 text-right">
                     <h4>개발자 문의</h4>
                     <h5>박경만 zzangman2@gmail.com</h5>
                 </section>

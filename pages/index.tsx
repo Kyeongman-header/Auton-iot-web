1f221315de58ac6d4ff1052c8b26ae797f93f712
Kyeongman <49219336+Kyeongman-header@users.noreply.github.com>
2022-02-01 18:05:07 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -517,7 +517,7 @@ export const getStaticProps :GetStaticProps=async(context)=> {
   let selectedids: string[] =[];
   
   try{
-    const machine_list = await fetch( `http://auton-iot.com/api/machine/`,
+    const machine_list = await fetch( `https://auton-iot.com/api/machine/`,
   {
     method: 'GET',
     headers:{
